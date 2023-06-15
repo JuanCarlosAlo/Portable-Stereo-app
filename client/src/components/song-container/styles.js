3abcfa1a915eee547d0632ad59fa3d87f1d44749
JC <jc.alonsome@gmail.com>
2023-06-15 13:39:23 +0200

    song cont mob

--- client/src/components/song-container/styles.js
+++ client/src/components/song-container/styles.js
@@ -12,7 +12,7 @@ const StyledTitle = styled.p`
 	/* animation: movingtitle 5s infinite alternate linear; */
 	overflow: hidden;
 	text-overflow: ellipsis;
-	max-width: 130px;
+	max-width: 140px;
 	@media screen and (min-width: 478px) {
 		max-width: fit-content;
 	}
