f9f6f424442e0ec4b595507bc0a34dd8e2b069d8
JC <jc.alonsome@gmail.com>
2023-06-18 17:36:08 +0200

    urls

--- client/src/provider/Auth.provider.jsx
+++ client/src/provider/Auth.provider.jsx
@@ -24,9 +24,7 @@ export const AuthProvider = ({ children }) => {
 	}, []);
 
 	useEffect(() => {
-		const socket = io(
-			'https://portable-stereo-app-production.up.railway.app:4000'
-		);
+		const socket = io('https://portable-stereo-app.onrender.com');
 
 		socket.on('collectionUsersChange', async change => {
 			switch (change.operationType) {
@@ -65,7 +63,7 @@ const getUserInfoFromMongo = async (
 ) => {
 	try {
 		const response = await fetch(
-			`https://portable-stereo-app-production.up.railway.app/portable-stereo/users/${user.uid}`
+			`https://portable-stereo-app.onrender.com/portable-stereo/users/${user.uid}`
 		);
 		if (response.ok) {
 			const userInfo = await response.json();
