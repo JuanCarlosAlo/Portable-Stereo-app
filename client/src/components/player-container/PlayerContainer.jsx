43ba6f4ae34370234d72b023a17d79778cf69c84
JC <jc.alonsome@gmail.com>
2023-06-19 12:36:34 +0200

    song localstorage

--- client/src/components/player-container/PlayerContainer.jsx
+++ client/src/components/player-container/PlayerContainer.jsx
@@ -1,4 +1,4 @@
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { StyledPlayerContainer } from './styles';
 import { SongContext } from '../../context/Song.context';
 
@@ -6,7 +6,34 @@ import Player from '../player/Player';
 
 const PlayerContainer = () => {
 	const { songData } = useContext(SongContext);
+	const [localStorageLoaded, setLocalStorageLoaded] = useState(false);
+
+	// Obtener los datos guardados en el localStorage al cargar la página
+	useEffect(() => {
+		const storedState = JSON.parse(localStorage.getItem('playerState'));
+		if (storedState) {
+			songData.setSongData({
+				song: songData.song,
+				index: storedState.songIndex
+			});
+			setLocalStorageLoaded(true);
+		}
+	}, []);
+
+	// Guardar los datos en el localStorage al cambiar la canción
+	useEffect(() => {
+		if (localStorageLoaded) {
+			localStorage.setItem(
+				'playerState',
+				JSON.stringify({
+					songIndex: songData.index
+				})
+			);
+		}
+	}, [songData.index, localStorageLoaded]);
+
 	if (!songData) return;
+
 	const file = songData.song;
 	const index = songData.index;
 
